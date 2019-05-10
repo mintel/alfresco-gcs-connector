a1a9cd20e0b83b77519588abfd7e23309fdbfb1a
Samuel Cheshire <scheshire@mintel.com>
2019-05-10 11:43:53 +0100

    test read

--- src/main/java/com/mintel/gcs/Main.java
+++ src/main/java/com/mintel/gcs/Main.java
@@ -1,9 +1,15 @@
 package com.mintel.gcs;
 
 import java.io.InputStream;
+import java.nio.ByteBuffer;
+import java.nio.channels.Channels;
 
 import com.google.auth.oauth2.GoogleCredentials;
+import com.google.cloud.storage.Blob;
+import com.google.cloud.storage.Bucket;
 import com.google.cloud.storage.Storage;
+import com.google.cloud.storage.Storage.BlobGetOption;
+import com.google.cloud.storage.Storage.BlobListOption;
 import com.google.cloud.storage.StorageOptions;
 
 public class Main
@@ -14,7 +20,38 @@ public class Main
         InputStream is = Main.class.getClassLoader().getResourceAsStream("key.json");
         GoogleCredentials credentials = GoogleCredentials.fromStream(is);
         Storage storage = StorageOptions.newBuilder().setCredentials(credentials).setProjectId("***REMOVED***").build().getService();
-        System.out.print(storage.get("***REMOVED***"));
+        System.out.println(storage.get("***REMOVED***"));
+
+        Bucket bucket = storage.get("***REMOVED***");
+        System.out.println(bucket);
+        BlobGetOption BLOB_FIELDS = BlobGetOption.fields(
+            Storage.BlobField.UPDATED,
+            Storage.BlobField.SIZE
+        );
+        for (Blob b : bucket.list().getValues())
+        {
+            System.out.println(b.getName());
+        }
+        Blob metadata = bucket.get("hi/sam.txt", BLOB_FIELDS);
+        System.out.println(metadata);
+        System.out.println(metadata.exists());
+        System.out.println(metadata.getUpdateTime());
+        System.out.println(metadata.getSize());
+        is = Channels.newInputStream(metadata.reader());
+        int a;
+        while (true)
+        {
+            a = is.read();
+            if (a != -1)
+            {
+                System.out.print((char) a);
+            }
+            else
+            {
+                break;
+            }
+        }
+        is.close();
     }
 
 }
