61c489876c23c3a0edc1ff3f936b16843409db2d
Willem Van den Eynde <wvandeneynde@mintel.com>
2019-10-21 08:31:52 +0100

    CMS-9478 S3 and GCS connectors don't work properly without local contentCache
    Use temporary file to guess mimetype instead of trying to catch file
    from objectstorage

--- src/test/java/com/mintel/gcs/GCSContentStoreIntegrationTest.java
+++ src/test/java/com/mintel/gcs/GCSContentStoreIntegrationTest.java
@@ -76,6 +76,34 @@ public class GCSContentStoreIntegrationTest extends AbstractAlfrescoIT
         assertFalse("Made up contenturl shouldn't exist", store.getReader("store://2099/9/17/3/58/182ad6bb-ec25-4012-8bcf-f370b2452d3e.bin").exists());
     }
 
+    /***
+     * Tests putContent method.
+     *
+     * We test this because we had 2 known issues:
+     *  ADMRemoteStore uses it. Without contentcache on, creating sites don't work anymore
+     *  Custom code at client that uses the exact same methods
+     *
+     *  It shouldn't throw an error, and obviously it should write the content
+     */
+    @Test
+    public void putContentTest() {
+        NodeRef parentRef = this.getCompanyHomeNodeRef();
+        String fileName = "putContentTest"+System.currentTimeMillis()+".txt";
+        String content = "putContentTest content";
+        FileInfo file = this.getServiceRegistry().getFileFolderService().create(
+            parentRef,
+            fileName,
+            ContentModel.TYPE_CONTENT,
+            ContentModel.ASSOC_CONTAINS);
+        NodeRef nodeRef = file.getNodeRef();
+        ContentWriter writer = this.getServiceRegistry().getContentService().getWriter(nodeRef, ContentModel.PROP_CONTENT, true);
+        writer.guessMimetype(fileName);
+        writer.putContent(content);
+
+        assertEquals(content, readTextContent(nodeRef));
+        assertEquals(MimetypeMap.MIMETYPE_TEXT_PLAIN, getServiceRegistry().getContentService().getReader(nodeRef, ContentModel.PROP_CONTENT).getMimetype());
+    }
+
     private NodeRef createTestNode(String content)
     {
         NodeRef parentRef = this.getCompanyHomeNodeRef();
